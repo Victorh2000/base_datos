0a5763f30530a6a427f7948a461e116988504345
Victor Martinez <victorh2000@Victors-MacBook-Pro.local>
2020-06-03 18:30:34 -0700

    Se agrego el registrations

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+// importamos express sqlite3 body-parser sequelize method-override
+// express.js es el framework para node.js
+// sqlite3 es para el de base de datos en SQL
+// sequielize es un ORM para node.js Object-Relational Mapping
+// para conectar a diferentes base de datos mysql, sqlite, postgres, mssql
+// methodOverride  es un modulo de nodejs para usar verbos como PUT or DELETE 
 const express = require('express');
 const sqlite3 = require('sqlite3');
 const bodyParser = require('body-parser');
@@ -7,6 +13,8 @@ const methodOverride = require('method-override');
 const app = express();
 
 const tasksRoutes = require('./routes/tasks_routes');
+const registrationRoutes = require('./routes/registrations_routes');
+
 
 //const tasks = require('./controllers/tasks');
 
@@ -17,6 +25,8 @@ app.use(methodOverride('_method'));
 app.set('view engine','pug');
 
 app.use(tasksRoutes);
+app.use(registrationRoutes);
+
 
 
 
